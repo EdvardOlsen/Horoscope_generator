d9790822f7d8098e08c1e646db47dd539e74c532
EdvardOlsen <83424516+EdvardOlsen@users.noreply.github.com>
2021-05-01 11:48:23 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,3 +2,4 @@ pip3 install -r requirements.txt
 git clone https://github.com/huggingface/transformers.git
 cd transformers/ && git checkout v2.2.0 
 wget https://github.com/dsnam/markovscope/raw/master/data/horoscopes.csv
+python3 prepare_data.py
