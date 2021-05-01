94e85e1ffb93e9fb430ba51be8215d552d778ba2
EdvardOlsen <83424516+EdvardOlsen@users.noreply.github.com>
2021-05-01 12:13:07 +0100

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -3,6 +3,7 @@ import telebot
 from urllib.request import urlretrieve
 import os
 import random
+from generate import generateHoro
 
     
 bot = telebot.TeleBot(token)
@@ -10,6 +11,6 @@ bot = telebot.TeleBot(token)
 def handle(message, i=0): 
     try:
       print('request recieved')
-      bot.send_message(chat_id=message.chat.id, text=generate_horo())
-    except:
+      bot.send_message(chat_id=message.chat.id, text=generateHoro(message.text))
+    except ValueError:
       bot.send_message(chat_id=message.chat.id, text='error')
